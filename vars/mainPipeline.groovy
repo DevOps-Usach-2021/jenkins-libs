0264579e8685451c7834f6ecd3b00cee37798eda
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-21 01:19:27 -0300

    printing env

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,4 +1,5 @@
 def call(Map config) {
+    echo "${env}"
     if (env.GIT_BRANCH.startsWith('origin/develop') || env.GIT_BRANCH.startsWith('origin/feature')) {
         echo "Branch: ${env.GIT_BRANCH}"
         ciPipeline
