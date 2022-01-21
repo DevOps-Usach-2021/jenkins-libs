0a9478c2e3cbc84a96767b986a51af8c73b2e446
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-21 00:53:05 -0300

    testing general pieline

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,9 +1,19 @@
-def init() {
-    // if ("${GIT_BRANCH}".startsWith('origin/develop') || "${GIT_BRANCH}".startsWith('origin/feature')) {
-        ciPipeline()
-    // }
-
-    // if ("${GIT_BRANCH}".startsWith('origin/release')) {
-    //     cdPipeline([:])
-    // }
+def call(Map config) {
+    script {
+        sh "echo 'hello'"
+    }
 }
+
+
+
+
+
+// def init() {
+//     // if ("${GIT_BRANCH}".startsWith('origin/develop') || "${GIT_BRANCH}".startsWith('origin/feature')) {
+//         ciPipeline()
+//     // }
+
+//     // if ("${GIT_BRANCH}".startsWith('origin/release')) {
+//     //     cdPipeline([:])
+//     // }
+// }
