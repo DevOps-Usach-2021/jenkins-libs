93ef87d2db18a977604775d61898d5ed9738f914
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-21 01:15:23 -0300

    using env.BRANCH_NAME

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,9 +1,6 @@
 def call(Map config) {
-    sh 'printenv'
-    echo "Branch: ${env.GIT_BRANCH}"
-
-    if (env.GIT_BRANCH.startsWith('origin/develop') || env.GIT_BRANCH.startsWith('origin/feature')) {
-        echo "Branch: ${env.GIT_BRANCH}"
+    if (env.BRANCH_NAME.startsWith('origin/develop') || env.BRANCH_NAME.startsWith('origin/feature')) {
+        echo "Branch: ${env.BRANCH_NAME}"
         ciPipeline
     }
 }
