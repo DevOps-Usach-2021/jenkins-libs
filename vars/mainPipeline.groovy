370d4861c4dba7eb8b57d41b956acfed0c633063
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-21 20:15:38 -0300

    fixing post identation

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -6,101 +6,100 @@ if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-')) {
         stages {
 
 
-                stage('1. Print env') {
-                    steps {
-                        script {
-                            utils.printEnv()
-                        }
+            stage('1. Print env') {
+                steps {
+                    script {
+                        utils.printEnv()
                     }
                 }
+            }
 
-                stage('Paso 2: Compliar') {
-                    when {
-                        // expression {
-                        //     GIT_BRANCH = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
-                        //     return (GIT_BRANCH.startsWith('develop') || GIT_BRANCH.startsWith('feature-'))
-                        // }
-                        expression {
-                            return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
-                        }
-                    }
-                    steps {
-                        script {
-                            sh "echo 'Compile Code!'"
-                            // Run Maven on a Unix agent.
-                            sh 'mvn clean compile -e'
-                        }
+            stage('Paso 2: Compliar') {
+                when {
+                    // expression {
+                    //     GIT_BRANCH = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+                    //     return (GIT_BRANCH.startsWith('develop') || GIT_BRANCH.startsWith('feature-'))
+                    // }
+                    expression {
+                        return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
                     }
                 }
-
-                stage('Paso 3: Testear') {
-                    when {
-                        expression {
-                            return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
-                        }
-                    }
-                    steps {
-                        script {
-                            sh "echo 'Test Code!'"
-                            // Run Maven on a Unix agent.
-                            sh 'mvn clean test -e'
-                        }
+                steps {
+                    script {
+                        sh "echo 'Compile Code!'"
+                        // Run Maven on a Unix agent.
+                        sh 'mvn clean compile -e'
                     }
                 }
+            }
 
-                stage('Paso 4: Build .Jar') {
-                    when {
-                        expression {
-                            return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
-                        }
-                    }
-                    steps {
-                        script {
-                            sh "echo 'Build .Jar!'"
-                            // Run Maven on a Unix agent.
-                            sh 'mvn clean package -e'
-                        }
+            stage('Paso 3: Testear') {
+                when {
+                    expression {
+                        return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
                     }
                 }
-
-                stage('Paso 5: SonarQube analysis') {
-                    when {
-                        expression {
-                            return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
-                        }
-                    }
-                    steps {
-                        withSonarQubeEnv('SonarQubeUsach') { // You can override the credential to be used
-                            sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=lab3-ci-develop'
-                        }
+                steps {
+                    script {
+                        sh "echo 'Test Code!'"
+                        // Run Maven on a Unix agent.
+                        sh 'mvn clean test -e'
                     }
                 }
+            }
 
-                stage('Paso 6: Levantar Springboot APP') {
-                    when {
-                        branch 'release-*'
-                    }
-                    steps {
-                        sh 'mvn spring-boot:run &'
+            stage('Paso 4: Build .Jar') {
+                when {
+                    expression {
+                        return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
                     }
                 }
-
-                stage('Paso 7: Dormir(Esperar 10sg) ') {
-                    when {
-                        branch 'release-*'
-                    }
-                    steps {
-                        sh 'sleep 100'
+                steps {
+                    script {
+                        sh "echo 'Build .Jar!'"
+                        // Run Maven on a Unix agent.
+                        sh 'mvn clean package -e'
                     }
                 }
+            }
 
-                stage('Paso 8: Test Alive Service - Testing Application!') {
-                    when {
-                        branch 'release-*'
+            stage('Paso 5: SonarQube analysis') {
+                when {
+                    expression {
+                        return env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-');
                     }
-                    steps {
-                        sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
+                }
+                steps {
+                    withSonarQubeEnv('SonarQubeUsach') { // You can override the credential to be used
+                        sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=lab3-ci-develop'
                     }
+                }
+            }
+
+            stage('Paso 6: Levantar Springboot APP') {
+                when {
+                    branch 'release-*'
+                }
+                steps {
+                    sh 'mvn spring-boot:run &'
+                }
+            }
+
+            stage('Paso 7: Dormir(Esperar 10sg) ') {
+                when {
+                    branch 'release-*'
+                }
+                steps {
+                    sh 'sleep 100'
+                }
+            }
+
+            stage('Paso 8: Test Alive Service - Testing Application!') {
+                when {
+                    branch 'release-*'
+                }
+                steps {
+                    sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
                 }
             }
         }
