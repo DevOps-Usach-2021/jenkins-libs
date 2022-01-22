8c0745ee53561e4af61fb23edaedaeb22df0e4bb
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-22 18:57:11 -0300

    testing ciWebhookPipeline

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,9 +1,10 @@
 // mainOipeline.groovy
 def call(Map pipelineParams) {
-    if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-')) {
-        ciPipeline()
-    }
-    if (env.BRANCH_NAME.startsWith('release-')) {
-        cdPipeline()
-    }
+    ciWebhookPipeline()
+    // if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-')) {
+    //     ciPipeline()
+    // }
+    // if (env.BRANCH_NAME.startsWith('release-')) {
+    //     cdPipeline()
+    // }
 }
