e677802b6456332fd75840656b9186f2b0f7bd21
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-22 14:24:09 -0300

    removing author from review

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -64,7 +64,7 @@ def call() {
                         script: """curl -X POST -d '{"title":"new feature: $BRANCH_NAME ","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'""",
                         returnStdout: true
                     ).trim()
-                    sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","fernandogutierrez27","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo"]}'"""
+                    sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo"]}'"""
                 }
             }
             failure {
