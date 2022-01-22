ec95ac1a8fc2141c9a33ae9e058203a7eb6e8139
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-22 14:13:34 -0300

    adding reviewers

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -64,7 +64,7 @@ def call() {
                         script: """curl -X POST -d '{"title":"new feature: $BRANCH_NAME ","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'""",
                         returnStdout: true
                     ).trim()
-                    sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","fgutierrez27"]}'"""
+                    sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","fgutierrez27","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo"]}'"""
                 }
             }
             failure {
