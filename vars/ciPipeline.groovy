0b3694702e77720452cc4a6f108297109af654ad
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-22 14:03:26 -0300

    adding step for success execution

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -58,12 +58,14 @@ def call() {
         }
         post {
             success {
-                sh "echo 'CI pipeline success'"
-                PR_NUMBER = sh (
-                    script: """curl -X POST -d '{"title":"new feature: $BRANCH_NAME ","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls""",
-                    returnStdout: true
-                ).trim()
-                sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","fgutierrez27"]}'"""
+                step {
+                    sh "echo 'CI pipeline success'"
+                    PR_NUMBER = sh (
+                        script: """curl -X POST -d '{"title":"new feature: $BRANCH_NAME ","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls""",
+                        returnStdout: true
+                    ).trim()
+                    sh """curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","fgutierrez27"]}'"""
+                }
             }
             failure {
                 sh "echo 'CI pipeline failure'"
