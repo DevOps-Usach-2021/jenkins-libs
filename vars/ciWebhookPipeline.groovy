5b78d55460bea2a23dcbf4fa21529284c864e4de
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-22 21:52:25 -0300

    updating refexpFilterText

--- vars/ciWebhookPipeline.groovy
+++ vars/ciWebhookPipeline.groovy
@@ -5,12 +5,13 @@ def call() {
         triggers {
             GenericTrigger(
                 genericVariables: [
-                    [key: 'ref', value: '$.ref']
+                    [key: 'ref', value: '$.ref'],
+                    [key: 'repository', regexpFilter: '[^a-z_-]', value: '$.repository']
                 ],
                 causeString: 'Triggered on $ref',
                 token: 'abc123',
                 regexpFilterExpression: '',
-                regexpFilterText: '*/'+env.BRANCH_NAME,
+                regexpFilterText: '$repository refs/heads/' + BRANCH_NAME,
                 printContributedVariables: true,
                 printPostContent: true
             )
