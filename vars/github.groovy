4527d67f2576c5a7b044e051985452c2a9553460
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-25 22:11:30 -0300

    fixing PR, must evaluate security

--- vars/github.groovy
+++ vars/github.groovy
@@ -2,22 +2,22 @@ def createPullRequest() {
     sh "echo 'CI pipeline success'"
     PR_NUMBER = sh (
         script: 
-            '''
-                curl -X POST -d '{"title":"new feature: ${BRANCH_NAME}","head":"${BRANCH_NAME}","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'
-            ''',
+            """
+                curl -X POST -d '{"title":"new feature: $BRANCH_NAME","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'
+            """,
         returnStdout: true
     ).trim()
-    sh '''
+    sh """
         curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo", "fernandogutierrez27"]}'
-    '''
+    """
 }
 
 def Map getCommitPayload() {
     def payload = sh (
         script: 
-        '''
+        """
             curl -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$REPOSITORY/commits/$GIT_COMMIT
-        ''',
+        """,
         returnStdout: true
     ).trim()
 
