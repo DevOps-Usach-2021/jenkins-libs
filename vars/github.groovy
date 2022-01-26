e9ea15b4936f8e1cd3dc577c88aedb07ed001ba1
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-25 21:53:29 -0300

    fixing broken PR feature

--- vars/github.groovy
+++ vars/github.groovy
@@ -3,12 +3,12 @@ def createPullRequest() {
     PR_NUMBER = sh (
         script: 
             '''
-                curl -X POST -d '{"title":"new feature: $BRANCH_NAME ","head":"$BRANCH_NAME","base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'
+                curl -X POST -d '{"title":"new feature: '$BRANCH_NAME' ","head":'$BRANCH_NAME',"base":"develop"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls | jq '.number'
             ''',
         returnStdout: true
     ).trim()
     sh '''
-        curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo","fernandogutierrez27"]}'
+        curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DevOps-Usach-2021/ms-iclab/pulls/$PR_NUMBER/requested_reviewers -d '{"reviewers":["jesusdonoso","anguitait", "carlostognarell", "MFrizR", "MrOscarDanilo", "fernandogutierrez27"]}'
     '''
 }
 
