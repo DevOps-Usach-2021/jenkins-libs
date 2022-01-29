faa57d71dd10e0f6179a732c1385e52921f2d2d1
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-29 09:19:54 -0300

    testing one pipeline, different stages

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,132 +1,108 @@
 // ciPipeline.groovy
 def call() {
-    pipeline {
-        agent any
-        environment {
-            GITHUB_TOKEN = credentials('github-token')
-        }
-        stages {
-
-            stage('1. Load environment') {
-                steps {
-                    script {
-                        utils.printEnv()
-                        env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
-                        PAYLOAD = github.getCommitPayload()
-                        def payload = utils.parseJson(PAYLOAD)
-                        currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
-                        currentBuild.description = payload.commit.message
-                        env.ARTIFACT_VERSION = utils.getVersionFromCommit(payload.commit.message)
-                        print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
-                    }
-                }
-                post {
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
-            }
-
-            stage('Paso 2: Compliar') {
-                steps {
-                    script {
-                        maven.compile()
-                    }
-                }
-                post {
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
-            }
-
-            stage('Paso 3: Testear') {
-                steps {
-                    script {
-                        maven.test()
-                    }
-                }
-                post {
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
-            }
-
-            stage('Paso 4: Build .Jar') {
-                steps {
-                    script {
-                        maven.buildJar()
-                    }
-                }
-                post {
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
-            }
-
-            stage('Paso 5: SonarQube analysis') {
-                steps {
-                    script {
-                        sonarqube.analyze()
-                    }
-                }
-                post {
-                    //record the test results and archive the jar file.
-                    success {
-                        script {
-                            nexus.uploadArtifact()
-                        }
-                    }
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
-            }
-
-            stage('Paso 6: Generar rama Release') {
-                when {
-                    branch 'develop'
-                }
-                steps {
-                    script {
-                        sh "echo 'Generando rama release'"
-                        github.createReleaseBranch()
-                    }
-                }
-                post {
-                    failure {
-                        script {
-                            env.FAIL_STAGE_NAME = STAGE_NAME
-                        }
-                    }
-                }
+    stage('1. Load environment') {
+        steps {
+            script {
+                utils.printEnv()
+                env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
+                PAYLOAD = github.getCommitPayload()
+                def payload = utils.parseJson(PAYLOAD)
+                currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
+                currentBuild.description = payload.commit.message
+                env.ARTIFACT_VERSION = utils.getVersionFromCommit(payload.commit.message)
+                print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
             }
         }
         post {
-            always {
-                sendNotifications currentBuild.result
+            failure {
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
             }
+        }
+    }
+
+    stage('Paso 2: Compliar') {
+        steps {
+            script {
+                maven.compile()
+            }
+        }
+        post {
+            failure {
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
+            }
+        }
+    }
+
+    stage('Paso 3: Testear') {
+        steps {
+            script {
+                maven.test()
+            }
+        }
+        post {
+            failure {
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
+            }
+        }
+    }
+
+    stage('Paso 4: Build .Jar') {
+        steps {
+            script {
+                maven.buildJar()
+            }
+        }
+        post {
+            failure {
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
+            }
+        }
+    }
+
+    stage('Paso 5: SonarQube analysis') {
+        steps {
+            script {
+                sonarqube.analyze()
+            }
+        }
+        post {
+            //record the test results and archive the jar file.
             success {
                 script {
-                    if (env.BRANCH_NAME.startsWith('feature-')) {
-                        github.createPullRequest()
-                    }
+                    nexus.uploadArtifact()
                 }
             }
             failure {
-                sh "echo 'CI pipeline failure'"
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
+            }
+        }
+    }
+
+    stage('Paso 6: Generar rama Release') {
+        when {
+            branch 'develop'
+        }
+        steps {
+            script {
+                sh "echo 'Generando rama release'"
+                github.createReleaseBranch()
+            }
+        }
+        post {
+            failure {
+                script {
+                    env.FAIL_STAGE_NAME = STAGE_NAME
+                }
             }
         }
     }
--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,9 +1,33 @@
 // mainPipeline.groovy
+// ciPipeline.groovy
 def call(Map pipelineParams) {
-    if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-')) {
-        ciPipeline()
-    }
-    if (env.BRANCH_NAME.startsWith('release-')) {
-        cdPipeline()
+    pipeline {
+        agent any
+        environment {
+            GITHUB_TOKEN = credentials('github-token')
+        }
+        stages {
+            if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME.startsWith('feature-')) {
+                ciPipeline()
+            }
+            if (env.BRANCH_NAME.startsWith('release-')) {
+                cdPipeline()
+            }
+        }
+        post {
+            always {
+                sendNotifications currentBuild.result
+            }
+            success {
+                script {
+                    if (env.BRANCH_NAME.startsWith('feature-')) {
+                        github.createPullRequest()
+                    }
+                }
+            }
+            failure {
+                sh "echo 'Pipeline failure'"
+            }
+        }
     }
 }
