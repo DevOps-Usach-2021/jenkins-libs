07f9b5ee2e44017f06406626f25782e21bd34abe
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-29 09:39:40 -0300

    removing steps and scripts

--- vars/cdPipeline.groovy
+++ vars/cdPipeline.groovy
@@ -1,113 +1,88 @@
 // cdPipeline.groovy
 def call() {
     stage('1. Load environment') {
-        steps {
-            script {
-                utils.printEnv()
-                env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
-                PAYLOAD = github.getCommitPayload()
-                def payload = utils.parseJson(PAYLOAD)
-                currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
-                currentBuild.description = payload.commit.message
-                env.ARTIFACT_VERSION = utils.getVersionFromBranch(BRANCH_NAME)
-                // print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        utils.printEnv()
+        env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
+        PAYLOAD = github.getCommitPayload()
+        def payload = utils.parseJson(PAYLOAD)
+        currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
+        currentBuild.description = payload.commit.message
+        env.ARTIFACT_VERSION = utils.getVersionFromBranch(BRANCH_NAME)
+        // print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 2: Download Artifact') {
-        steps {
-            script {
-                nexus.downloadArtifact()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        nexus.downloadArtifact()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 3: Levantar Springboot APP') {
-        steps {
-            script {
-                maven.runApp()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        maven.runApp()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 4: Dormir(Esperar 10sg) ') {
-        steps {
-            sh 'sleep 10'
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        sh 'sleep 10'
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 5: Test Alive Service - Testing Application!') {
-        steps {
-            sh 'curl -X GET "http://localhost:8082/rest/mscovid/test?msg=testing"'
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        sh 'curl -X GET "http://localhost:8082/rest/mscovid/test?msg=testing"'
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 6: Merge to main') {
-        steps {
-            script {
-                // github.mergeBranch('develop')
-                github.mergeBranch('main')
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        // github.mergeBranch('develop')
+        github.mergeBranch('main')
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 7: Tag') {
-        steps {
-            script {
-                // github.mergeBranch('develop')
-                github.tagMainBranch()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        github.tagMainBranch()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 }
 
--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,78 +1,58 @@
 // ciPipeline.groovy
 def call() {
     stage('1. Load environment') {
-        steps {
-            script {
-                utils.printEnv()
-                env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
-                PAYLOAD = github.getCommitPayload()
-                def payload = utils.parseJson(PAYLOAD)
-                currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
-                currentBuild.description = payload.commit.message
-                env.ARTIFACT_VERSION = utils.getVersionFromCommit(payload.commit.message)
-                print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        utils.printEnv()
+        env.REPOSITORY = GIT_URL.split('github.com/')[1].split('.git')[0]
+        PAYLOAD = github.getCommitPayload()
+        def payload = utils.parseJson(PAYLOAD)
+        currentBuild.displayName = REPOSITORY + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
+        currentBuild.description = payload.commit.message
+        env.ARTIFACT_VERSION = utils.getVersionFromCommit(payload.commit.message)
+        print ("ARTIFACT_VERSION: " + ARTIFACT_VERSION)
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 2: Compliar') {
-        steps {
-            script {
-                maven.compile()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        maven.compile()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 3: Testear') {
-        steps {
-            script {
-                maven.test()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        maven.test()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 4: Build .Jar') {
-        steps {
-            script {
-                maven.buildJar()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        maven.buildJar()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 
     stage('Paso 5: SonarQube analysis') {
-        steps {
-            script {
-                sonarqube.analyze()
-            }
-        }
+        sonarqube.analyze()
         post {
             //record the test results and archive the jar file.
             success {
@@ -92,18 +72,14 @@ def call() {
         when {
             branch 'develop'
         }
-        steps {
-            script {
-                sh "echo 'Generando rama release'"
-                github.createReleaseBranch()
-            }
-        }
-        post {
-            failure {
-                script {
-                    env.FAIL_STAGE_NAME = STAGE_NAME
-                }
-            }
-        }
+        sh "echo 'Generando rama release'"
+        github.createReleaseBranch()
+        // post {
+        //     failure {
+        //         script {
+        //             env.FAIL_STAGE_NAME = STAGE_NAME
+        //         }
+        //     }
+        // }
     }
 }
